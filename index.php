fb89ab8e70b651c96e8753c556fc181c60079f71
osamaa <osamaa@CG-WS-030517.clickandgo.local>
2017-06-21 16:14:22 +0100

    Master

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ 
+
+<body>
+ <?php 
+
+echo '<p>Hello World</p>'; 
+$x = 5 /* + 15 */ + 5;
+echo $x;
+
+$color = "red";
+
+//echo "My car is " . $color . "<br>";
+$txt1 = "Learn PHP";
+
+echo "<h2>$txt1</h2>";
+
+
+?> 
+</body>
 </html>
